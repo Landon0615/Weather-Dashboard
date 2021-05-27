857e71eaa37a5f198d8738b46aface163133fef0
Landon Waddell <Landon.waddell@gmail.com>
2021-05-26 22:23:56 -0600

    Added get uvINdec function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let button = document.querySelector('.button')
-let inputValue = document.querySelector('.inputValue')
+
 let name =  document.querySelector('.name')
 let wind =  document.querySelector('.wind')
 let temp =  document.querySelector('.temp')
@@ -9,27 +9,44 @@ const weatherKey = "f4048cf303566008248577ccd1cef8a8"
 
 
 button.addEventListener('click',function(){
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=f4048cf303566008248577ccd1cef8a8&units=imperial')
+    let inputValue = document.querySelector('.inputValue').value
+currentWeather(inputValue)
+
+})
+    function currentWeather(cityName) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f4048cf303566008248577ccd1cef8a8&units=imperial`)
     .then(response => response.json())
     .then(data => {
         let nameValue = data['name'];
         let dateValue = data['timezone'];
         let tempVAlue = data['main']['temp'];
         let windValue = data['wind']['speed'];
-        let uvValue = data[''];
+        
         
 
         name.innerHTML = nameValue;
         temp.innerHTML = tempVAlue;
         wind.innerHTML = windValue;
         date.innerHTML = dateValue;
-        UV.innerHTML = uvValue;
+        getUvIndex()
 console.log(data);
     })
     
     .catch(err => alert("Wrong city name!"))
-})
+}
 
+function getUvIndex(lat,lon) {
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=f4048cf303566008248577ccd1cef8a8&units=imperial`)
+    .then(response => response.json())
+    .then(data => {
+
+console.log(data)
+    // let uvValue = data[''];
+    // UV.innerHTML = uvValue;
+})
+}
+
+//
 
 
 // function fiveDayForecast(){
@@ -37,7 +54,6 @@ console.log(data);
 //     let apiKey = "f4048cf303566008248577ccd1cef8a8"
 
 
-//     var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=San+Diego&appid=" + apiKey;
+    // var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=San+Diego&appid=" + apiKey;
     
     
-  
